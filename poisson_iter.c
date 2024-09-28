a1b9f0b7e10b51d1f7db12cc10e0eda99da9f905
Jack Duignan <jdu80@uclive.ac.nz>
2024-09-29 09:32:53 +1300

    Von neuman boundary updating correctly

--- poisson_iter.c
+++ poisson_iter.c
@@ -26,48 +26,68 @@ void apply_const_boundary(int N, double* next) {
 
 void apply_von_neuman_boundary_slice(int N, double* source, double* curr, double* next, float delta, slice3D_t slice_3D) {
     for (int k = slice_3D.k_start; k < slice_3D.k_end; k++) {
-        idx(next, N, k, 0, 0) = (2 * idx(curr, N, k, 0, 0 + 1)
-            + 2 * idx(curr, N, k, 0 + 1, 0)
-            + idx(curr, N, k + 1, 0, 0) + idx(curr, N, k - 1, 0, 0)
-            - delta * delta * idx(source, N, k, 0, 0)) / 6;
-
-        idx(next, N, k, N - 1, N - 1) = (2 * idx(curr, N, k, N - 1, N - 1 - 1)
-            + 2 * idx(curr, N, k, N - 1 - 1, N - 1)
-            + idx(curr, N, k + 1, N - 1, N - 1) + idx(curr, N, k - 1, N - 1, N - 1)
-            - delta * delta * idx(source, N, k, N - 1, N - 1)) / 6;
-
-        idx(next, N, k, N - 1, 0) = (2 * idx(curr, N, k, N - 1, 0 + 1)
-            + 2 * idx(curr, N, k, N - 1 - 1, 0)
-            + idx(curr, N, k + 1, N - 1, 0) + idx(curr, N, k - 1, N - 1, 0)
-            - delta * delta * idx(source, N, k, N - 1, 0)) / 6;
-
-        idx(next, N, k, 0, N - 1) = (2 * idx(curr, N, k, 0, N - 1 - 1)
-            + 2 * idx(curr, N, k, 0 + 1, N - 1)
-            + idx(curr, N, k + 1, 0, N - 1) + idx(curr, N, k - 1, 0, N - 1)
-            - delta * delta * idx(source, N, k, 0, N - 1)) / 6;
-
-        for (int j = slice_3D.j_start + 1; j < slice_3D.j_end - 1; j++) {
-            idx(next, N, k, j, 0) = (2 * idx(curr, N, k, j, 0 + 1)
-                + idx(curr, N, k, j + 1, 0) + idx(curr, N, k, j - 1, 0)
-                + idx(curr, N, k + 1, j, 0) + idx(curr, N, k - 1, j, 0)
-                - delta * delta * idx(source, N, k, j, 0)) / 6;
-
-            idx(next, N, k, j, N - 1) = (2 * idx(curr, N, k, j, N - 1 - 1)
-                + idx(curr, N, k, j + 1, N - 1) + idx(curr, N, k, j - 1, N - 1)
-                + idx(curr, N, k + 1, j, N - 1) + idx(curr, N, k - 1, j, N - 1)
-                - delta * delta * idx(source, N, k, j, N - 1)) / 6;
+        if (slice_3D.j_start == 0 && slice_3D.i_start == 0) {
+            idx(next, N, k, 0, 0) = (2 * idx(curr, N, k, 0, 0 + 1)
+                + 2 * idx(curr, N, k, 0 + 1, 0)
+                + idx(curr, N, k + 1, 0, 0) + idx(curr, N, k - 1, 0, 0)
+                - delta * delta * idx(source, N, k, 0, 0)) / 6;
         }
 
-        for (int i = slice_3D.i_start + 1; i < slice_3D.i_end - 1; i++) {
-            idx(next, N, k, 0, i) = (idx(curr, N, k, 0, i + 1) + idx(curr, N, k, 0, i - 1)
-                + 2 * idx(curr, N, k, 0 + 1, i)
-                + idx(curr, N, k + 1, 0, i) + idx(curr, N, k - 1, 0, i)
-                - delta * delta * idx(source, N, k, 0, i)) / 6;
+        if (slice_3D.j_end == N && slice_3D.j_end == N) {
+            idx(next, N, k, N - 1, N - 1) = (2 * idx(curr, N, k, N - 1, N - 1 - 1)
+                + 2 * idx(curr, N, k, N - 1 - 1, N - 1)
+                + idx(curr, N, k + 1, N - 1, N - 1) + idx(curr, N, k - 1, N - 1, N - 1)
+                - delta * delta * idx(source, N, k, N - 1, N - 1)) / 6;
+        }
 
-            idx(next, N, k, N - 1, i) = (idx(curr, N, k, N - 1, i + 1) + idx(curr, N, k, N - 1, i - 1)
-                + 2 * idx(curr, N, k, N - 1 - 1, i)
-                + idx(curr, N, k + 1, N - 1, i) + idx(curr, N, k - 1, N - 1, i)
-                - delta * delta * idx(source, N, k, N - 1, i)) / 6;
+        if (slice_3D.j_end == N && slice_3D.i_start == 0) {
+            idx(next, N, k, N - 1, 0) = (2 * idx(curr, N, k, N - 1, 0 + 1)
+                + 2 * idx(curr, N, k, N - 1 - 1, 0)
+                + idx(curr, N, k + 1, N - 1, 0) + idx(curr, N, k - 1, N - 1, 0)
+                - delta * delta * idx(source, N, k, N - 1, 0)) / 6;
+        }
+
+        if (slice_3D.j_start == 0 && slice_3D.j_end == N) {
+            idx(next, N, k, 0, N - 1) = (2 * idx(curr, N, k, 0, N - 1 - 1)
+                + 2 * idx(curr, N, k, 0 + 1, N - 1)
+                + idx(curr, N, k + 1, 0, N - 1) + idx(curr, N, k - 1, 0, N - 1)
+                - delta * delta * idx(source, N, k, 0, N - 1)) / 6;
+        }
+    
+        if (slice_3D.i_start == 0) {
+            for (int j = slice_3D.j_start + 1; j < slice_3D.j_end - 1; j++) {
+                idx(next, N, k, j, 0) = (2 * idx(curr, N, k, j, 0 + 1)
+                    + idx(curr, N, k, j + 1, 0) + idx(curr, N, k, j - 1, 0)
+                    + idx(curr, N, k + 1, j, 0) + idx(curr, N, k - 1, j, 0)
+                    - delta * delta * idx(source, N, k, j, 0)) / 6;
+            }
+        }
+
+        if (slice_3D.i_end == N) {
+            for (int j = slice_3D.j_start + 1; j < slice_3D.j_end - 1; j++) {
+                idx(next, N, k, j, N - 1) = (2 * idx(curr, N, k, j, N - 1 - 1)
+                    + idx(curr, N, k, j + 1, N - 1) + idx(curr, N, k, j - 1, N - 1)
+                    + idx(curr, N, k + 1, j, N - 1) + idx(curr, N, k - 1, j, N - 1)
+                    - delta * delta * idx(source, N, k, j, N - 1)) / 6;
+            }
+        }
+
+        if (slice_3D.j_start == 0) {
+            for (int i = slice_3D.i_start + 1; i < slice_3D.i_end - 1; i++) {
+                idx(next, N, k, 0, i) = (idx(curr, N, k, 0, i + 1) + idx(curr, N, k, 0, i - 1)
+                    + 2 * idx(curr, N, k, 0 + 1, i)
+                    + idx(curr, N, k + 1, 0, i) + idx(curr, N, k - 1, 0, i)
+                    - delta * delta * idx(source, N, k, 0, i)) / 6;
+            }
+        }
+
+        if (slice_3D.j_end == N) {
+            for (int i = slice_3D.i_start + 1; i < slice_3D.i_end - 1; i++) {
+                idx(next, N, k, N - 1, i) = (idx(curr, N, k, N - 1, i + 1) + idx(curr, N, k, N - 1, i - 1)
+                    + 2 * idx(curr, N, k, N - 1 - 1, i)
+                    + idx(curr, N, k + 1, N - 1, i) + idx(curr, N, k - 1, N - 1, i)
+                    - delta * delta * idx(source, N, k, N - 1, i)) / 6;
+            }
         }
     }
 }
